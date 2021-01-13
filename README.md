035e446767611b4553581475893698e42667c041
anawat <alonypau@gmail.com>
2021-01-13 15:04:53 +0700

    add README

---
+++ README.md
@@ -0,0 +1,13 @@
+### Classroom Smart Box with Face ID
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `python run.py`
+
+You can change process scale in line 8
+
+### `python save.py` 
+
+Change file name in line 11 and name in line 20 before run
--- save.py
+++ save.py
@@ -12,10 +12,8 @@ inputFrame = cv2.imread('./picture/anawat.jpg')
 faceBoxes, faceLandmarks, alignedFaces = detector.detect(inputFrame)
 extractEmbedding = embeddingExtractor.extract(alignedFaces)
 embed = np.load('./storeEmbedding/embedding.npy', allow_pickle=True)
-# embed = []
 embed = np.append(embed, [extractEmbedding])
 np.save('./storeEmbedding/embedding.npy', embed)
 name = np.load('./storeEmbedding/name.npy', allow_pickle=True)
-# name = []
 name = np.append(name, ['Anawat'])
 np.save('./storeEmbedding/name.npy', name)
